027e88dff62c4c0d274b1d8eba74a62c50eb8f3e
Sebastian Narvaez R. <sebasnr95@protonmail.com>
2026-02-24 19:08:27 -0500

    Added drop statements to Create tables.

--- templates/postgres/create_tables.tmplt.sql
+++ templates/postgres/create_tables.tmplt.sql
@@ -1,8 +1,13 @@
-CREATE SCHEMA IF NOT EXISTS ${gbl["project_name"]}
-	% for model in models:
-    CREATE TABLE ${pascalc(model["name"])} (
-        % for prop in model["props"]:
-        ${pascalc(prop["name"])} ${conv["sql"][prop["type"]]} ${"NULL" if "optional" in prop and not prop["optional"] else ""}${"," if not loop.last else ""}
-        % endfor
-    )${";" if loop.last else ""}
+CREATE SCHEMA IF NOT EXISTS ${gbl["project_name"]};
+
+% for model in models:
+-- DROP TABLE IF EXISTS ${gbl["project_name"]}.${pascalc(model["name"])};
+% endfor
+
+% for model in models:
+CREATE TABLE ${gbl["project_name"]}.${pascalc(model["name"])} (
+    % for prop in model["props"]:
+    ${"," if not loop.first else " "}${pascalc(prop["name"])} ${conv["sql"][prop["type"]]}${" NULL" if "optional" in prop and not prop["optional"] else ""}
     % endfor
+);
+% endfor
