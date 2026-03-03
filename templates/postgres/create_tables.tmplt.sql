7cb71bd081e2dcfce31efa01bc94f27cbc9a87e0
Sebastian Narvaez R. <sebasnr95@protonmail.com>
2026-03-02 21:19:50 -0500

    Transitioned models to dict in templates

--- templates/postgres/create_tables.tmplt.sql
+++ templates/postgres/create_tables.tmplt.sql
@@ -1,10 +1,10 @@
 CREATE SCHEMA IF NOT EXISTS ${gbl["project_name"]};
 
-% for model in models:
+% for model in models.values():
 -- DROP TABLE IF EXISTS ${gbl["project_name"]}.${pascalc(model["name"])};
 % endfor
 
-% for model in models:
+% for model in models.values():
 CREATE TABLE ${gbl["project_name"]}.${pascalc(model["name"])} (
     % for prop in model["props"]:
     ${"," if not loop.first else " "}${pascalc(prop["name"])} ${conv["sql"][prop["type"]]}${" NULL" if "optional" in prop and not prop["optional"] else ""}
