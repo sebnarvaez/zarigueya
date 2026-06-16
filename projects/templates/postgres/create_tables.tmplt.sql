130184f5cf7a1a49bfc7725d6af4d0e735292fd7
Sebastian Narvaez R. <sebasnr95@protonmail.com>
2026-06-16 11:48:18 -0500

    Created templates for create tables and seed data

---
+++ projects/templates/postgres/create_tables.tmplt.sql
@@ -0,0 +1,18 @@
+IF EXISTS(SELECT datname FROM pg_catalog.pg_database WHERE LOWER(datname) = LOWER('${gbl["project_name"]}')
+BEGIN
+    CREATE DATABASE ${gbl["project_name"]};
+END
+\c ${gbl["project_name"]}
+
+% for model in models.values():
+-- DROP TABLE IF EXISTS ${pascalc(model["name"])};
+% endfor
+
+% for model in models.values():
+CREATE TABLE IF NOT EXISTS ${pascalc(model["name"])} (
+    % for prop in model["props"]:
+    ${"," if not loop.first else " "}${pascalc(prop["name"])} ${conv["sql"][prop["type"]]}${" NULL" if "optional" in prop and not prop["optional"] else ""}
+    % endfor
+);
+
+% endfor
